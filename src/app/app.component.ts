9ab3482544fc67896d20b5303ca5fae00b4fba93
mohammednoorulameen <noorulameen8606>
2021-12-28 18:36:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,104 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'calculator';
+  input: string = '';
+  result: string = '';
+
+  pressNum(num: string) {
+    if (num == '.') {
+      if (this.input != '') {
+        const lastNum = this.getLastOperant();
+        console.log(lastNum.lastIndexOf('.'));
+        if (lastNum.lastIndexOf('.') >= 0) return;
+      }
+    }
+
+    if (num == '0') {
+      if (this.input == '') {
+        return;
+      }
+
+      const prevKey = this.input[this.input.length - 1];
+      if (
+        prevKey === '/' ||
+        prevKey === '*' ||
+        prevKey === '-' ||
+        prevKey === '+'
+      ) {
+        return;
+      }
+    }
+    this.input = this.input + num;
+    this.calcAnswer();
+  }
+  getLastOperant() {
+    let pos: number;
+    console.log(this.input);
+    pos = this.input.toString().lastIndexOf('+');
+    if (this.input.toString().lastIndexOf('-') > pos)
+      pos = this.input.lastIndexOf('-');
+    if (this.input.toString().lastIndexOf('*') > pos)
+      pos = this.input.lastIndexOf('*');
+    if (this.input.toString().lastIndexOf('/') > pos)
+      pos = this.input.lastIndexOf('/');
+    console.log('last ' + this.input.substr(pos + 1));
+    return this.input.substr(pos + 1);
+  }
+
+  pressOperator(op: string) {
+    const lastKey = this.input[this.input.length - 1];
+    if (
+      lastKey === '/' ||
+      lastKey === '*' ||
+      lastKey === '-' ||
+      lastKey === '+'
+    ) {
+      return;
+    }
+
+    this.input = this.input + op;
+    this.calcAnswer();
+  }
+  clear() {
+    if (this.input != '') {
+      this.input = this.input.substr(0, this.input.length - 1);
+    }
+  }
+  allclear() {
+    this.result = '';
+    this.input = '';
+  }
+
+  calcAnswer() {
+    let formula = this.input;
+
+    let lastkey = formula[formula.length - 1];
+
+    if (lastkey === '.') {
+      formula = formula.substr(0, formula.length - 1);
+    }
+
+    lastkey = formula[formula.length - 1];
+
+    if (
+      lastkey === '/' ||
+      lastkey === '*' ||
+      lastkey === '-' ||
+      lastkey === '+' ||
+      lastkey === '.'
+    ) {
+      formula = formula.substr(0, formula.length - 1);
+    }
+
+    console.log('formula ' + formula);
+    this.result = eval(formula);
+  }
+  getAnswer() {
+    this.calcAnswer();
+    this.input = this.result;
+    if (this.input == '0') this.input = '';
+  }
 }
